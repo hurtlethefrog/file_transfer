31c8e2c3e108c9259d0b31cf2e2a8fdd2fd6884b
Duncan H <duncan.haran@gmail.com>
2020-01-05 17:22:32 -0500

    added close connection to error message, encoding and decoding responses

--- client.py
+++ client.py
@@ -3,35 +3,37 @@ import socket
 
 def exe():
     host = "127.0.0.1"
-    port = 5000
+    port = 5003
 
     s = socket.socket()
     s.connect((host, port))
 
     filename = input("Filename? :")
     if filename != 'Q':
-        s.send(filename)
-        data = s.recv(1024)
+        s.send(filename.encode())
+        data = s.recv(1024).decode()
         if data[:11] == "FILE EXISTS":
-            filesize = data[19:]
+            filesize = int(data[19:])
             message = input(
                 f"File Size:{str(filesize)}Bytes. Download? (Y/N) -- ")
             if message == "Y":
-                s.send('Downloading')
-                f = open(f'new_{filename}wb')
+                s.send('Downloading'.encode())
+                f = open(f'new_{filename}', "wb")
                 data = s.recv(1024)
                 totalSize = len(data)
                 f.write(data)
                 while totalSize < filesize:
                     data = s.recv(1024)
-                    totalSize = + len(data)
+                    totalSize += len(data)
                     f.write(data)
-                    print(f"{(totalSize/float(filesize))*100}%")
+                    print("{0:.2f}".format((totalSize / float(filesize)) * 100) + "%")
                 print("Done")
+                s.close()
         else:
             print("File does not exist")
+            s.close()
     s.close()
 
 
-if __name__ == "__Main__":
+if __name__ == "__main__":
     exe()
