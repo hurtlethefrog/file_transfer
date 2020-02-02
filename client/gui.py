a4267b33c7e444e490fad9efc4385a8de4a06532
Duncan H <duncan.haran@gmail.com>
2020-02-02 13:26:23 -0500

    README changes pulled from master

--- client/gui.py
+++ client/gui.py
@@ -1,5 +1,17 @@
 from tkinter import *
-from random import *
+
+class HoverButton(Button):
+    def __init__(self, master, **kw):
+        Button.__init__(self,master=master,**kw)
+        self.defaultText = self["text"]
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_enter(self, e):
+        self['text'] = self['filesize']
+
+    def on_leave(self, e):
+        self['text'] = self.defaultText
 
 root = Tk()
 
@@ -7,7 +19,7 @@ root.title("Magnificent file stealing app")
 
 
 for i in range(1, 10):
-    num = random()
-    Button(root, text=num, width=10).grid(row=i, column=0, sticky=W)
+    listButton = HoverButton(root, text=i, width=15)
+    listButton.grid(row=i, column=0, sticky=W)
 
 root.mainloop()
