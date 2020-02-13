6175fb29025edc2f80d04e504f7f2e0412418747
Duncan H <duncan.haran@gmail.com>
2020-02-13 14:57:26 -0500

    sends filename:filsize as dict

--- database/gui-server.py
+++ database/gui-server.py
@@ -25,27 +25,27 @@ def retrieve_file(name, sock):
     #         sock.send("ERR".encode())
     # elif search == "N":
     print("sending dirlist")
-    filenames = []
+    filenames = {}
     with os.scandir("./") as db:
         for entry in db:
+            size = str(os.path.getsize(entry.name))
             if entry.name != "server.py" or "gui-server.py":
-                filenames.append(entry.name)
-    # weird pickle thing
+                filenames.update({entry.name : size})
     sock.send(pickle.dumps(filenames))
-    # copy and pasted code QQ, bad duncan
     filename = sock.recv(1024).decode()
+    # if statement from non GUI server file, could be removed
     if filename != "server.py" or "gui-server.py":
-        if os.path.isfile(filename):
-            sock.send(
-                f"FILE EXISTS : SIZE {str(os.path.getsize(filename))}".encode())
-            user_response = sock.recv(1024).decode()
-            if user_response[:2] == "go":
-                with open(filename, "rb") as f:
-                    bytes_to_send = f.read(1024)
-                    sock.send(bytes_to_send)
-                    while bytes_to_send != "":
-                        bytes_to_send = f.read(1024)
-                        sock.send(bytes_to_send)
+        # if os.path.isfile(filename):
+        #     sock.send(
+        #         f"FILE EXISTS : SIZE {str(os.path.getsize(filename))}".encode())
+        #     user_response = sock.recv(1024).decode()
+        #     if user_response[:2] == "go":
+        with open(filename, "rb") as f:
+            bytes_to_send = f.read(1024)
+            sock.send(bytes_to_send)
+            while bytes_to_send != "":
+                bytes_to_send = f.read(1024)
+                sock.send(bytes_to_send)
 
     sock.close()
 
