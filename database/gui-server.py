f567e5e1d5a82bb141fd05961984d5f594c78936
Duncan H <duncan.haran@gmail.com>
2020-02-13 14:33:44 -0500

    send dirlist on server connection

--- database/gui-server.py
+++ database/gui-server.py
@@ -5,47 +5,47 @@ import pickle
 
 
 def retrieve_file(name, sock):
-    print("getting file")
-    search = sock.recv(1024).decode()
-    if search == "Y":
-        filename = sock.recv(1024).decode()
-        if filename != "server.py":
-            if os.path.isfile(filename):
-                sock.send(
-                    f"FILE EXISTS : SIZE {str(os.path.getsize(filename))}".encode())
-                user_response = sock.recv(1024).decode()
-                if user_response[:2] == "go":
-                    with open(filename, "rb") as f:
+    # print("getting file")
+    # search = sock.recv(1024).decode()
+    # if search == "Y":
+    #     filename = sock.recv(1024).decode()
+    #     if filename != "server.py":
+    #         if os.path.isfile(filename):
+    #             sock.send(
+    #                 f"FILE EXISTS : SIZE {str(os.path.getsize(filename))}".encode())
+    #             user_response = sock.recv(1024).decode()
+    #             if user_response[:2] == "go":
+    #                 with open(filename, "rb") as f:
+    #                     bytes_to_send = f.read(1024)
+    #                     sock.send(bytes_to_send)
+    #                     while bytes_to_send != "":
+    #                         bytes_to_send = f.read(1024)
+    #                         sock.send(bytes_to_send)
+    #     else:
+    #         sock.send("ERR".encode())
+    # elif search == "N":
+    print("sending dirlist")
+    filenames = []
+    with os.scandir("./") as db:
+        for entry in db:
+            if entry.name != "server.py" or "gui-server.py":
+                filenames.append(entry.name)
+    # weird pickle thing
+    sock.send(pickle.dumps(filenames))
+    # copy and pasted code QQ, bad duncan
+    filename = sock.recv(1024).decode()
+    if filename != "server.py" or "gui-server.py":
+        if os.path.isfile(filename):
+            sock.send(
+                f"FILE EXISTS : SIZE {str(os.path.getsize(filename))}".encode())
+            user_response = sock.recv(1024).decode()
+            if user_response[:2] == "go":
+                with open(filename, "rb") as f:
+                    bytes_to_send = f.read(1024)
+                    sock.send(bytes_to_send)
+                    while bytes_to_send != "":
                         bytes_to_send = f.read(1024)
                         sock.send(bytes_to_send)
-                        while bytes_to_send != "":
-                            bytes_to_send = f.read(1024)
-                            sock.send(bytes_to_send)
-        else:
-            sock.send("ERR".encode())
-    elif search == "N":
-        print("sending dirlist")
-        filenames = []
-        with os.scandir("./") as db:
-            for entry in db:
-                if entry.name != "server.py":
-                    filenames.append(entry.name)
-        # weird pickle thing
-        sock.send(pickle.dumps(filenames))
-        # copy and pasted code QQ, bad duncan
-        filename = sock.recv(1024).decode()
-        if filename != "server.py":
-            if os.path.isfile(filename):
-                sock.send(
-                    f"FILE EXISTS : SIZE {str(os.path.getsize(filename))}".encode())
-                user_response = sock.recv(1024).decode()
-                if user_response[:2] == "go":
-                    with open(filename, "rb") as f:
-                        bytes_to_send = f.read(1024)
-                        sock.send(bytes_to_send)
-                        while bytes_to_send != "":
-                            bytes_to_send = f.read(1024)
-                            sock.send(bytes_to_send)
 
     sock.close()
 
